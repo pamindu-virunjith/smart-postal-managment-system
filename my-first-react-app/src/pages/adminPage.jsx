cb09c9a1e06cd5f40d6ab3e12ea448e4c136f2b5
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-07-28 09:56:19 +0530

    enhanced user view of admin page

--- my-first-react-app/src/pages/adminPage.jsx
+++ my-first-react-app/src/pages/adminPage.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 
 import { FaUsers } from "react-icons/fa";
 import { MdOutlineWarehouse } from "react-icons/md";
@@ -10,12 +10,17 @@ import EditParcel from "./admin/editParcel";
 
 
 export default function AdminPage() {
+
+    const location = useLocation();
+    const path = location.pathname;
+
+
     return (
         <div className='w-full h-screen flex bg-gray-100 p-2'>
             <div className='h-full w-[300px] bg-red-200 rounded-xl shadow-lg p-4 flex flex-col mr-2'>
-                <Link to ="/admin/users" className=" p-2 flex items-center text-xl hover:bg-red-300 rounded-xl" > <FaUsers className="mr-2 text-blue-700"/> Users</Link>
-                <Link to ="/admin/parcel" className=" p-2 flex items-center text-xl hover:bg-red-300 rounded-xl"> <MdOutlineWarehouse className="mr-2 text-amber-950"/> Parcel</Link>
-                <Link to ="/admin/orders" className=" p-2 flex items-center text-xl hover:bg-red-300 rounded-xl"> <LiaFileInvoiceSolid className="mr-2 text-rose-600"/> Orders</Link>
+                <Link to ="/admin/users" className={`p-2 my-0.5 flex items-center text-xl hover:bg-red-300 rounded-xl ${path === "/admin/users" ? "bg-red-300" : ""}`} > <FaUsers className="mr-2 text-blue-700"/> Users</Link>
+                <Link to ="/admin/parcel" className={`p-2 my-0.5 flex items-center text-xl hover:bg-red-300 rounded-xl ${path === "/admin/parcel" ? "bg-red-300" : ""}`} > <MdOutlineWarehouse className="mr-2 text-green-500"/> Parcel</Link>
+                <Link to ="/admin/orders" className={`p-2 my-0.5 flex items-center text-xl hover:bg-red-300 rounded-xl ${path === "/admin/orders" ? "bg-red-300" : ""}`} > <LiaFileInvoiceSolid className="mr-2 text-rose-600"/> Orders</Link>
             </div>
 
             <div className="h-full bg-white w-[calc(100vw-300px)] rounded-xl shadow-lg p-4 overflow-y-auto">
