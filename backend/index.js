27ca62de1f129019f75683c0c8b3762aa7e61eec
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-09-03 10:47:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://smart-postal-managment-system.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true
     }
@@ -23,7 +23,7 @@ const io = new Server(server, {
 
 // Enable CORS for all routes
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://smart-postal-managment-system.vercel.app"],
     credentials: true
 }));
 
